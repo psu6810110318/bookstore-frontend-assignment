cfda0be38eaddeb2de411574b25b15604c6dd425
Warit Phetkaeo <6810110318@psu.ac.th>
2025-12-17 22:49:05 +0700

    add react router (declarative)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import axios from 'axios'
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginScreen from './LoginScreen';
 import BookScreen from './BookScreen';
 
@@ -8,7 +9,10 @@ axios.defaults.baseURL = "http://localhost:3000"
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const handleLoginSuccess = () => {setIsAuthenticated(true)}
+  
+  const handleLoginSuccess = () => {
+    setIsAuthenticated(true)
+  }
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -19,9 +23,19 @@ function App() {
   }, [])
 
   return (
-    <>
-      {isAuthenticated ? <BookScreen/> : <LoginScreen onLoginSuccess={handleLoginSuccess}/>}
-    </>
+    <Router>
+      <Routes>
+        <Route 
+          path="/login" 
+          element={!isAuthenticated ? <LoginScreen onLoginSuccess={handleLoginSuccess}/> : <Navigate to="/books" />} 
+        />
+        <Route 
+          path="/books" 
+          element={isAuthenticated ? <BookScreen/> : <Navigate to="/login" />} 
+        />
+        <Route path="*" element={<Navigate to={isAuthenticated ? "/books" : "/login"} />} />
+      </Routes>
+    </Router>
   )
 }
 
